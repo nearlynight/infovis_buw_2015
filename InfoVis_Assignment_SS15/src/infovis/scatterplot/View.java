a946f5d777bb952bcb3f7e32267071ab57e9da6d
Till Fastnacht <t.fastnacht@googlemail.com>
2015-05-31 19:37:18 +0200

    markerRectangle doesn't cross scatter tile border

--- InfoVis_Assignment_SS15/src/infovis/scatterplot/View.java
+++ InfoVis_Assignment_SS15/src/infovis/scatterplot/View.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Point2D;
 
 import javax.swing.JPanel;
 
@@ -14,9 +15,9 @@ public class View extends JPanel {
 	     private Model model = null;
 	     private Rectangle2D markerRectangle = new Rectangle2D.Double(0,0,0,0); 
 	     private final int boxSize = 150;
-	     private final int smallBoxSize = 147;
-	     private final int outerSpace = 50;
 	     private final int circleSize = 3;
+	     private final int smallBoxSize = boxSize-circleSize;
+	     private final int outerSpace = 50;     
 
 		 public Rectangle2D getMarkerRectangle() {
 			return markerRectangle;
@@ -26,9 +27,10 @@ public class View extends JPanel {
 		public void paint(Graphics g) {
 			Graphics2D g2D = (Graphics2D) g;
 			g2D.clearRect(0, 0, getWidth(), getHeight());
-			g2D.setColor(Color.BLACK);
-			g2D.fill(markerRectangle);
 			g2D.draw(markerRectangle);
+			g2D.setColor(Color.RED);
+			g2D.draw(markerRectangle);
+			g2D.setColor(Color.BLACK);
 
 			for (int i = 0; i<model.getDim(); ++i) {
 				for (int j = 0; j<model.getDim(); ++j) {
@@ -39,47 +41,74 @@ public class View extends JPanel {
 					g2D.drawString(l, boxSize*i+outerSpace+10, -35);
 					g2D.rotate(-Math.PI*.5);
 					for (int k = 0; k < model.getList().size(); ++k) {
-						double valueX = model.getList().get(k).getValues()[i];
-						double valueY = model.getList().get(k).getValues()[j];
-						
-						double maxX = model.getRanges().get(i).getMax();
-						double maxY = model.getRanges().get(j).getMax();
-						double minX = model.getRanges().get(i).getMin();
-						double minY = model.getRanges().get(j).getMin();
-						
-						double posX = 1 - ((maxX-valueX) / (maxX-minX));
-						double posY = 1 - ((maxY-valueY) / (maxY-minY));
-						int pointX = outerSpace + boxSize*i + (int)(smallBoxSize * posX);
-						int pointY = outerSpace + boxSize*j + (int)(smallBoxSize * posY);
-						g2D.drawRect(pointX, pointY, circleSize, circleSize);
+						Point2D myPoint = giveMePoint(i,j,k);						
+						g2D.drawRect((int)myPoint.getX(), (int)myPoint.getY(), circleSize, circleSize);
 					}
 				}	
-			}
-
-
-			
-			
-			/*
-	        for (String l : model.getLabels()) {
-				Debug.print(l);
-				Debug.print(",  ");
-				Debug.println("");
-			}
-			
-			for (Range range : model.getRanges()) {
-				Debug.print(range.toString());
-				Debug.print(",  ");
-				Debug.println("");
-			}
-			/*
-			for (Data d : model.getList()) {
-				Debug.print(d.toString());
-				Debug.println("");
-			}
-	        */
-			
+			}		
 		}
+		
 		public void setModel(Model model) {
 			this.model = model;
 		}
+		
+		public Point2D giveMePoint (int i, int j, int k) {
+			double valueX = model.getList().get(k).getValues()[i];
+			double valueY = model.getList().get(k).getValues()[j];
+			
+			double maxX = model.getRanges().get(i).getMax();
+			double maxY = model.getRanges().get(j).getMax();
+			double minX = model.getRanges().get(i).getMin();
+			double minY = model.getRanges().get(j).getMin();
+			
+			double posX = 1 - ((maxX-valueX) / (maxX-minX));
+			double posY = 1 - ((maxY-valueY) / (maxY-minY));
+			int pointX = outerSpace + boxSize*i + (int)(smallBoxSize * posX);
+			int pointY = outerSpace + boxSize*j + (int)(smallBoxSize * posY);
+			return new Point2D.Double(pointX, pointY);
+		}
+		
+		public void updateMarkerRect(int xPress, int yPress, int xDrag, int yDrag) {
+			Tile myTile = new Tile(xPress, yPress); 
+			xDrag = Math.max(myTile.getXMin(), xDrag);
+			xDrag = Math.min(myTile.getXMax(), xDrag);
+			yDrag = Math.max(myTile.getYMin(), yDrag);
+			yDrag = Math.min(myTile.getYMax(), yDrag);
+			int widthMin = Math.min(xPress, xDrag);
+			int heigthMin = Math.min(yPress, yDrag);
+			int widthMax = Math.max(xPress, xDrag);
+			int heigthMax = Math.max(yPress, yDrag);
+			int width = widthMax - widthMin;
+			int heigth = heigthMax - heigthMin;
+			markerRectangle.setRect(widthMin, heigthMin, width, heigth);
+
+			//int restrictX = restrictRectangleMarkerX(xPress, yPress, width, heigth);
+		}
+		
+		
+		public class Tile {
+			public int a;
+			public int b;
+			
+			public Tile (int xPress, int yPress) {
+				this.a = (xPress - outerSpace) / boxSize;
+				this.b = (yPress - outerSpace) / boxSize;
+			}
+			
+			public int getXMin() {
+				return a*boxSize+outerSpace;
+			}
+			public int getXMax() {
+				return a*boxSize+outerSpace+boxSize;
+			}
+			public int getYMin() {
+				return b*boxSize+outerSpace;
+			}
+			public int getYMax() {
+				return b*boxSize+outerSpace+boxSize;
+			}
+		}
 }
+
+
+
